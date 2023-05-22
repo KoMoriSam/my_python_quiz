e7d0b3e512346ce1b079f5d10bf293b37b646f35
KoMoriSam <1760665075@qq.com>
2023-05-22 19:33:41 +0800

    📃quiz_01

--- quiz_01.py
+++ quiz_01.py
@@ -1,4 +1,4 @@
-# python 中生成随机整数、随机小数、0--1之间小数方法
+# 014. python 中生成随机整数、随机小数、0--1之间小数方法
 # 随机整数 - random.randint(a,b), 生成区间内的整数
 # 随机小数 - 习惯用 numpy 库，利用 np.random.randn(5) 生成5个随机小数
 # 0~1随机小数 - random.random(), 括号中不传参
